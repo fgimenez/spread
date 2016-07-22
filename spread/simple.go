06d1afd7bec4a8909e3aa58ad83c13c46ccf6f71
Federico Gimenez <fgimenez@canonical.com>
2016-07-22 15:23:37 +0200

    initial provider file

---
+++ spread/simple.go
@@ -0,0 +1,67 @@
+package spread
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v2"
+)
+
+func Simple(b *Backend) Provider {
+	return &simple{b}
+}
+
+type simple struct {
+	backend *Backend
+}
+
+type simpleServer struct {
+	b *simple
+	d simpleServerData
+}
+
+type simpleServerData struct {
+	Name    string
+	Backend string
+	System  string
+	Address string
+}
+
+func (s *simpleServer) String() string {
+	return fmt.Sprintf("%s:%s (%s)", s.b.backend.Name, s.d.System, s.d.Name)
+}
+
+func (s *simpleServer) Provider() Provider {
+	return s.l
+}
+
+func (s *simpleServer) Address() string {
+	return s.d.Address
+}
+
+func (s *simpleServer) System() string {
+	return s.d.System
+}
+
+func (s *simpleServer) ReuseData() []byte {
+	data, err := yaml.Marshal(&s.d)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func (s *simpleServer) Discard() error {
+	return nil
+}
+
+func (s *simple) Backend() *Backend {
+	return s.backend
+}
+
+func (s *simple) Reuse(data []byte, password string) (Server, error) {
+	return &simpleServer{}, nil
+}
+
+func (s *simple) Allocate(system string, password string, keep bool) (Server, error) {
+	return &simpleServer{}, nil
+}
